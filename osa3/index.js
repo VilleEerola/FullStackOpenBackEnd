b7d091bc0cb2dd25e2f2a8e854c6ddde41eb0c77
Ville Eerola <ville.eerolaa@gmail.com>
2023-11-07 15:39:54 +0200

    tehty 3.1-3.5

---
+++ osa3/index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const application = express()
+application.use(express.json())
+
+// the list of persons in the phonebook
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramob",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122"
+  }
+]
+
+// sends "persons" to browser
+application.get('/api/persons', (req, res) =>{
+    res.json(persons)
+})
+
+// Prints the info of how many persons in the phonebook and 
+//current time when request was sent
+// sends that info to browser
+application.get('/api/info', (req, res) =>{
+    const timeNow = new Date()
+    const howManyPeople = `Phonebook has info for ${persons.length} people`
+    const response = 
+    `<html>
+        <body>
+            <p>${howManyPeople}</p>
+            <p>Current time: ${timeNow}</p>
+        </body>
+    </html>`
+    res.send(response)
+})
+
+application.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+  })
+  // send the DELETE request to server and deletes person from the phonebook
+  application.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+  })
+
+  const generateIdRandom = () => {
+    return Math.floor(Math.random() * 100000000000)
+  }
+  
+  application.post('/api/persons', (req, res) => {
+    const body = req.body
+  
+    if (!body.name || !body.number) {
+      return res.status(400).json({ 
+        error: 'name or number is missing' 
+      })
+    }
+  
+    const addPerson = {
+      name: body.name,
+      number: body.number,
+      id: generateIdRandom(),
+    }
+  
+    persons = persons.concat(addPerson)
+  
+    res.json(addPerson)
+  })
+
+
+
+
+const PORT = 3001
+application.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
